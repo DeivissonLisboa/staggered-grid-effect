73b418e513b579958543fbf3893d878960df7234
Deivisson Lisboa <dvilaflor@gmail.com>
2022-12-23 19:27:59 -0300

    create onClick handler and animejs stagger effect

--- script.js
+++ script.js
@@ -2,10 +2,37 @@ const tiles = document.getElementById("tiles")
 const tileSize = 50
 let columns = Math.floor(document.body.clientWidth / 50)
 let rows = Math.floor(document.body.clientHeight / 50)
+const colors = [
+  "#f94144",
+  "#f3722c",
+  "#f8961e",
+  "#f9844a",
+  "#f9c74f",
+  "#90be6d",
+  "#43aa8b",
+  "#4d908e",
+  "#577590",
+  "#277da1",
+]
+let counter = 0
+
+function handleClick(index) {
+  console.log(index)
+  anime({
+    targets: ".tile",
+    backgroundColor: colors[counter % (colors.length - 1)],
+    delay: anime.stagger(50, {
+      grid: [columns, rows],
+      from: index,
+    }),
+  })
+  counter++
+}
 
 function createTile(index) {
   const tile = document.createElement("div")
   tile.classList.add("tile")
+  tile.addEventListener("click", () => handleClick(index))
   return tile
 }
 
