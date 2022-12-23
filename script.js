8f3c088827a409147f24ac81ea578c89507fba00
Deivisson Lisboa <dvilaflor@gmail.com>
2022-12-23 18:54:27 -0300

    Create tiles function

---
+++ script.js
@@ -0,0 +1,17 @@
+const tiles = document.getElementById("tiles")
+let columns = Math.floor(document.body.clientWidth / 50),
+  rows = Math.floor(document.body.clientHeight / 50)
+
+function createTile(index) {
+  const tile = document.createElement("div")
+  tile.classList.add("tile")
+  return tile
+}
+
+function createTiles(number) {
+  Array.from(Array(number)).map((tile, index) => {
+    tiles.appendChild(createTile(index))
+  })
+}
+
+createTiles(columns * rows)
